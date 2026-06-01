688086a0da9fa65469f79ee209e14c0ea36ab811
Hai124-coder <haihenry1124@gmail.com>
2026-06-01 19:47:59 +0700

    Add Submit Task and Reject submit

---
+++ MangaManagementSystem.Business/DTOs/Requests/RejectPageTaskSubmissionRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangaManagementSystem.Business.DTOs.Requests
+{
+    public class RejectPageTaskSubmissionRequest
+    {
+        [Required]
+        [MaxLength(1000)]
+        public string RejectReason { get; set; } = null!;
+    }
+}
