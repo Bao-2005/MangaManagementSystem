eff12718c5a71b585ab578a420ef5b633b80e953
Hai124-coder <haihenry1124@gmail.com>
2026-06-01 19:43:56 +0700

    Add Page Task Response

---
+++ MangaManagementSystem.Business/DTOs/Responses/PageTaskResponse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaManagementSystem.Business.DTOs.Responses
+{
+    public class PageTaskResponse
+    {
+        public Guid PageTaskId { get; set; }
+
+        public Guid ChapterId { get; set; }
+
+        public string ChapterTitle { get; set; } = null!;
+
+        public Guid ManuscriptId { get; set; }
+
+        public Guid AssistantId { get; set; }
+
+        public string AssistantName { get; set; } = null!;
+
+        public int PageStart { get; set; }
+
+        public int PageEnd { get; set; }
+
+        public string TaskType { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? ApprovedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public IReadOnlyCollection<PageTaskSubmissionResponse> Submissions { get; set; } =
+            Array.Empty<PageTaskSubmissionResponse>();
+    }
+}
+
---
+++ MangaManagementSystem.Business/DTOs/Responses/PageTaskSubmissionResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaManagementSystem.Business.DTOs.Responses
+{
+    public class PageTaskSubmissionResponse
+    {
+        public Guid SubmissionId { get; set; }
+
+        public Guid PageTaskId { get; set; }
+
+        public int VersionNo { get; set; }
+
+        public Guid SubmittedFileAssetId { get; set; }
+
+        public string SubmittedFileName { get; set; } = null!;
+
+        public string Status { get; set; } = null!;
+
+        public string? Note { get; set; }
+
+        public string? RejectReason { get; set; }
+
+        public DateTime? SubmittedAt { get; set; }
+
+        public DateTime? ReviewedAt { get; set; }
+    }
+}
+
