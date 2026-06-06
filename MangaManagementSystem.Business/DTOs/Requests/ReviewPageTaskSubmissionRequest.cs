c88d46c9a3fd08426ca5b601a846f9692ca56fa1
Hai124-coder <haihenry1124@gmail.com>
2026-06-06 09:23:31 +0700

    Add Review Task Submit Request

---
+++ MangaManagementSystem.Business/DTOs/Requests/ReviewPageTaskSubmissionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MangaManagementSystem.Business.DTOs.Requests;
+
+public class ReviewPageTaskSubmissionRequest
+{
+    [MaxLength(1000)]
+    public string? RejectReason { get; set; }
+}
+
